f8ca4c2c2264f59822d64d5583e0e0de3d1a358c
jumacasni <jumacasni@gmail.com>
2021-10-20 21:21:02 +0200

    borro clases para reorganizar issues

--- playme/src/catalogo.py
+++
@@ -1,6 +0,0 @@
-from juego import Juego
-
-class Catalogo:
-	def __init__(self, id_catalogo, juegos):
-		self.id = id_catalogo
-		self.juegos = juegos
--- playme/src/juego.py
+++
@@ -1,4 +0,0 @@
-class Juego:
-	def __init__(self, id_juego, nombre):
-		self.id = id_juego
-		self.nombre = nombre
