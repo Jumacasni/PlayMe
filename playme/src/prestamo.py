4a82be566a648e622e5dd9ae7d92ba6084b06291
jumacasni <jumacasni@gmail.com>
2021-10-24 12:47:11 +0200

    añado atributo identificador de prestamo. Closes #52

--- playme/src/prestamo.py
+++ playme/src/prestamo.py
@@ -1,8 +1,13 @@
-from datetime import date
+from datetime import datetime
+import itertools
 
 class Prestamo:
+
+	id_iter = itertools.count()
+
 	def __init__(self, id_juego):
-		self.fecha_inicio = date.today()
+		self.id = next(self.id_iter)
+		self.fecha_inicio = datetime.now()
 		self.fecha_fin = None
 		self.id_juego = id_juego
-		self.estado = True
+		self.activo = True
