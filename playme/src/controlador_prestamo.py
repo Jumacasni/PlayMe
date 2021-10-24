1f0ef9d8d63f2069d36c1455665f354cf4e48f5f
jumacasni <jumacasni@gmail.com>
2021-10-24 12:53:54 +0200

    metodo para finalizar un prestamo. Avanzo #35

--- playme/src/controlador_prestamo.py
+++ playme/src/controlador_prestamo.py
@@ -9,3 +9,12 @@ class ControladorPrestamo:
 	def crear_prestamo(self, id_juego):
 		p = Prestamo(id_juego)
 		self.prestamos.append(p)
+
+	def finalizar_prestamo(self, prestamo):
+		if (prestamo.activo):
+			prestamo.activo = False
+			prestamo.fecha_fin = datetime.now()
+			return True
+
+		else:
+			return False
