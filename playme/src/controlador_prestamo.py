80bbc65b5f9c4bdabf78f82cc9b022dc4ea9e9bc
jumacasni <jumacasni@gmail.com>
2021-10-24 12:55:43 +0200

    metodo para calcular tiempo restante de un prestamo. Closes #36

--- playme/src/controlador_prestamo.py
+++ playme/src/controlador_prestamo.py
@@ -18,3 +18,15 @@ class ControladorPrestamo:
 
 		else:
 			return False
+
+	def tiempo_restante(self, prestamo):
+		if (prestamo.activo):
+			if (datetime.now() > prestamo.fecha_inicio):
+				diferencia_hora = datetime.now() - prestamo.fecha_inicio
+
+				# Devolver tiempo restante en formato %H:%M
+				return (str(diferencia_hora).split(".")[0])[:-3]
+
+			return None
+
+		return None
