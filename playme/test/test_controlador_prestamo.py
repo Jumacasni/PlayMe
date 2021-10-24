a31ae948d03ca1e5240df5e1e257099487a36362
jumacasni <jumacasni@gmail.com>
2021-10-24 16:28:58 +0200

    tests unitarios para la clase ControladorPrestamo. Avanzo #49

---
+++ playme/test/test_controlador_prestamo.py
@@ -0,0 +1,71 @@
+import sys
+import os
+sys.path.append('playme/src/')
+
+from prestamo import Prestamo
+from juego import Juego
+from controlador_prestamo import ControladorPrestamo
+from datetime import datetime
+import pytest
+
+def test_crear_prestamo():
+	controlador = ControladorPrestamo()
+	juego = Juego(1, "Aventureros al Tren")
+	prestamo = Prestamo(juego)
+
+	controlador.crear_prestamo(juego)
+
+	assert len(controlador.prestamos) == 1
+	assert prestamo.activo == True
+	assert prestamo.fecha_fin == None
+	assert type(prestamo.fecha_inicio) == datetime
+	assert type(prestamo) == Prestamo
+
+def test_finalizar_prestamo_activo():
+	controlador = ControladorPrestamo()
+	juego = Juego(1, "Aventureros al Tren")
+	prestamo = Prestamo(juego)
+
+	controlador.crear_prestamo(juego)
+	res = controlador.finalizar_prestamo(prestamo)
+
+	assert res == True
+	assert prestamo.activo == False
+	assert prestamo.fecha_fin != None
+	assert type(prestamo.fecha_fin) == datetime
+
+def test_finalizar_prestamo_inactivo():
+	controlador = ControladorPrestamo()
+	juego = Juego(1, "Aventureros al Tren")
+	prestamo = Prestamo(juego)
+
+	controlador.crear_prestamo(juego)
+	controlador.finalizar_prestamo(prestamo)
+	res = controlador.finalizar_prestamo(prestamo)
+
+	assert res == False
+
+def test_tiempo_restante_prestamo_activo():
+	controlador = ControladorPrestamo()
+	juego = Juego(1, "Aventureros al Tren")
+	prestamo = Prestamo(juego)
+
+	controlador.crear_prestamo(juego)
+	formato = "%H:%M"
+
+	res = controlador.tiempo_restante(prestamo)
+
+	assert datetime.strptime(res, formato)
+
+def test_tiempo_restante_prestamo_inactivo():
+	controlador = ControladorPrestamo()
+	juego = Juego(1, "Aventureros al Tren")
+	prestamo = Prestamo(juego)
+
+	controlador.crear_prestamo(juego)
+	formato = "%H:%M"
+
+	controlador.finalizar_prestamo(prestamo)
+	res = controlador.tiempo_restante(prestamo)
+
+	assert res == None
