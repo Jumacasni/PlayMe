aa347f238a7004e3fbc47767aac4705bdcc9ff19
jumacasni <jumacasni@gmail.com>
2021-11-14 18:53:10 +0100

    elimino atributo id de préstamo #62

--- playme/prestamo.py
+++ playme/prestamo.py
@@ -3,10 +3,7 @@ import itertools
 
 class Prestamo:
 
-	id_iter = itertools.count()
-
 	def __init__(self, juego):
-		self.id = next(self.id_iter)
 		self.fecha_inicio = datetime.now()
 		self.fecha_fin = None
 		self.juego = juego
