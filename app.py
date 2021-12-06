083d59d19daad5979eb97d276babfd8eb9401772
jumacasni <jumacasni@gmail.com>
2021-12-06 20:57:17 +0100

    [skip ci] Añado uvicorn para probar logs. Closes #80

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from routers import controlador_prestamo
 import etcd3
 
 app = FastAPI()
+
 app.include_router(controlador_prestamo.router)
 
 if __name__ == "__main__":
 	client = etcd3.client()
 
-	puerto = client.get('puerto')[0]
-	host = client.get('host')[0]
+	puerto = client.get('puerto')[0].decode('utf-8')
+	host = client.get('host')[0].decode('utf-8')
 	
+	uvicorn.run("app:app", host=host, port=puerto)
