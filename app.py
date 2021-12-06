dfb960efcb497b63649df48450c3dfd2265e631f
jumacasni <jumacasni@gmail.com>
2021-12-06 21:15:30 +0100

    [skip ci] Quito servidor de app.py

--- app.py
+++ app.py
@@ -6,11 +6,3 @@ import etcd3
 app = FastAPI()
 
 app.include_router(controlador_prestamo.router)
-
-if __name__ == "__main__":
-	client = etcd3.client()
-
-	puerto = client.get('puerto')[0].decode('utf-8')
-	host = client.get('host')[0].decode('utf-8')
-	
-	uvicorn.run("app:app", host=host, port=puerto)
