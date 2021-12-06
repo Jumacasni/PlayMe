0bd5758eb218b8fc1579cf3cb0db7991d22409c9
jumacasni <jumacasni@gmail.com>
2021-12-06 16:57:30 +0100

    [skip ci] Añade ruta para fecha fin de un préstamo #73

--- routers/prestamo.py
+++ routers/prestamo.py
@@ -27,3 +27,16 @@ def crear_prestamo(prestamo: PrestamoModel):
 	prestamo = Prestamo(Juego(prestamo.juego.id, prestamo.juego.nombre))
 	
 	return prestamo.get_fecha_inicio()
+
+@router.get("/fecha_fin")
+def crear_prestamo(prestamo: PrestamoModel):
+	pr = Prestamo(Juego(prestamo.juego.id, prestamo.juego.nombre))
+	pr.fecha_fin = prestamo.fecha_fin
+	pr.activo = prestamo.activo
+
+	res = pr.get_fecha_fin()
+
+	if res is None:
+		raise HTTPException(status_code=404, detail="El préstamo no ha finalizado aún")
+
+	return res
