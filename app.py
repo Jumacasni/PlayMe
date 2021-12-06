07e650e2728ffe9e1be8e9b58039bb0d755ee209
jumacasni <jumacasni@gmail.com>
2021-12-06 19:28:48 +0100

    [skip ci] Añado etcd3. Closes #79

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
 from routers import controlador_prestamo, prestamo
+import etcd3
 
 app = FastAPI()
 app.include_router(controlador_prestamo.router)
 app.include_router(prestamo.router)
+
+if __name__ == "__main__":
+	client = etcd3.client()
+
+	puerto = client.get('puerto')[0]
+	host = client.get('host')[0]
+	
