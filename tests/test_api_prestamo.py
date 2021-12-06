7f1c413e8a8395b70f27578d8733cb65f534c6fd
jumacasni <jumacasni@gmail.com>
2021-12-06 16:58:02 +0100

    Testea ruta de fecha fin de un préstamo. Closes #73

--- tests/test_api_prestamo.py
+++ tests/test_api_prestamo.py
@@ -17,3 +17,20 @@ def test_get_fecha_inicio():
 	)
 
 	assert_that(response.status_code).is_equal_to(200)
+
+def test_get_fecha_fin():
+	response = client.get("/prestamo/fecha_fin",
+		json={"fecha_fin": "2032-06-01T12:13:14", "juego": {"id": 1, "nombre": "Aventureros al Tren"}, "activo": False}
+	)
+
+	assert_that(response.status_code).is_equal_to(200)
+
+def test_get_fecha_fin_fail():
+	response = client.get("/prestamo/fecha_fin",
+		json={"juego": {"id": 1, "nombre": "Aventureros al Tren"}, "activo": True}
+	)
+
+	res = json.loads(response.text)
+
+	assert_that(response.status_code).is_equal_to(404)
+	assert_that(res["detail"]).is_equal_to("El préstamo no ha finalizado aún")
